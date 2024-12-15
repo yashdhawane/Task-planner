8c39dfab677afea6f66f08da8e2973c454f1a1a4
yashdhawane <yashdhawane17@gmail.com>
2024-12-15 17:26:25 +0530

    vite:fix

--- FE/task/vite.config.js
+++ FE/task/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   build: {
+    outDir: 'dist',
     rollupOptions: {
       external: ['axios'], // Explicitly mark axios as an external dependency
     },
