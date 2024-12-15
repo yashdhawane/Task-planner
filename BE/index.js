5b2ff6bf6eccce4fd12ff86d1fa26c302847c58f
yashdhawane <yashdhawane17@gmail.com>
2024-12-15 18:25:16 +0530

    change index

--- BE/index.js
+++ BE/index.js
@@ -19,25 +19,11 @@ app.use(express.json());
 
 if (NODE_ENV === 'production') {
     // Serve static files from frontend build (if applicable)
-    const distPath = path.resolve(__dirname, '../FE/task/dist');
-
-  if (!fs.existsSync(distPath)) {
-    console.error('Dist directory does not exist:', distPath);
-    process.exit(1); // Exit if the directory is missing
-  }
-
-  app.use(express.static(distPath));
-
-  // Handle React routing, return all requests to index.html
-  app.get('*', (req, res) => {
-    const indexPath = path.join(distPath, 'index.html');
-    if (!fs.existsSync(indexPath)) {
-      console.error('Index.html not found:', indexPath);
-      res.status(404).send('index.html not found');
-      return;
-    }
-    res.sendFile(indexPath);
-  });
+    // app.use(express.static(path.join(__dirname, '../FE/task/dist')));
+    app.get('/', (req, res) => {
+        // res.sendFile(path.join(__dirname, '../FE/task/dist', 'index.html'));
+        app.get('/', (req, res) => res.send('Backend is running!'));
+      });
   }
 
 try {
